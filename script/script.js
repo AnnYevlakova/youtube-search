cd7b4684a5cef433d376c8ebbfa0737560448cab
AnnYevlakova <ann.yevlakova@gmail.com>
2017-04-27 23:03:40 +0300

    youtube/feature: implement base logic and structure

--- script/script.js
+++ script/script.js
@@ -1,20 +1,26 @@
+var _ = require("lodash");
+
 function init() {
     gapi.client.setApiKey('AIzaSyB9Y-NxEUbkWCJq1Bu7LM2nCGhmHDEyzFE');
     gapi.client.load("youtube", "v3", function(){});
 }
-
+window.onload = function() {
+    init();
+};
 let next = '';
 let prev = '';
 let pages = [];
-var query = document.getElementById('enterQuery').value;
+let imgs = '';
+let query;
 document.getElementById('next').addEventListener('click', function() {search(next)});
 document.getElementById('prev').addEventListener('click', function() {search(prev)});
+document.getElementById('search-button').addEventListener('click', function() {search()});
 
 // Search for a specified string.
 function search(pageToken) {
     let request;
     let results;
-    
+    query = document.getElementById('enterQuery').value;
     if(pageToken != undefined) {
         request = gapi.client.youtube.search.list({
             part: "snippet",
@@ -41,5 +47,17 @@ function search(pageToken) {
             prev = '';
         }
         next = results.nextPageToken;
+        imgs = results.items.map(function(item){
+            return item.snippet.thumbnails.default.url;
+        });
+        let tmpl = _.template('<%obj.forEach(function(item){%>\
+                        <li class="default"><img src="<%-item%>" alt="" width="100px" height="100px">\
+                        </li><%});%>');
+        let html = tmpl(imgs);
+        document.getElementById('results').innerHTML = html;
     });
 }
+module.exports = {
+    imgs
+}
+
